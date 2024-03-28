7c1ac6aab8e1859227447dae684a9c5e8bdd4ef5
vanesa <vanesa.juarez.paris@gmail.com>
2024-03-28 12:59:19 +0100

    feat: :sparkles: add the reusable custom text widgets into the home class

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:character_creator/shared/styled_text.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -12,13 +13,13 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Your Characers'),
+        title: const StyledTitle('Your Characters'),
         centerTitle: true,
       ),
       body: Container(
           padding: const EdgeInsets.all(16),
           child: Column(children: [
-            const Text('Character List'),
+            const StyledText('Character List'),
             FilledButton(onPressed: () {}, child: const Text('Create New'))
           ])),
     );
