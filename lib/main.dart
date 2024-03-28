9e3ac6313674252ea0fb35c5641147e423ea0bb0
vanesa <vanesa.juarez.paris@gmail.com>
2024-03-28 12:56:59 +0100

    feat: :sparkles: add the custom theme to the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:character_creator/screens/home/home.dart';
+import 'package:character_creator/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Home(),
+  runApp(MaterialApp(
+    theme: primaryTheme,
+    home: const Home(),
   ));
 }
