97f5c970338901a6062ae147444a0a8f32c3f9e5
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-01 11:07:54 +0200

    feat: :sparkles: add the characterCard custom widget

---
+++ lib/screens/home/character_card.dart
@@ -0,0 +1,36 @@
+import 'package:character_creator/models/character.dart';
+import 'package:character_creator/shared/styled_text.dart';
+import 'package:character_creator/theme.dart';
+import 'package:flutter/material.dart';
+
+class CharacterCard extends StatelessWidget {
+  const CharacterCard(this.character, {super.key});
+
+  final Character character;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: Row(
+        children: [
+          Image.asset('assets/img/vocations/${character.vocation.image}',
+              width: 80),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              StyledHeading(character.name),
+              StyledText(character.vocation.title)
+            ],
+          ),
+          const Expanded(child: SizedBox()),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.arrow_forward, color: AppColors.textColor))
+        ],
+      ),
+    ));
+  }
+}
