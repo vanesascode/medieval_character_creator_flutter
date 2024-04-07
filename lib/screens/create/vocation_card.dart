9e7391e58269eb60abd3f0d977587a24ce154b40
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-07 20:54:57 +0200

    feat: :sparkles: add the onTap and the selected props to the VocationCard widget

--- lib/screens/create/vocation_card.dart
+++ lib/screens/create/vocation_card.dart
@@ -1,33 +1,46 @@
 import 'package:character_creator/models/vocation.dart';
 import 'package:character_creator/shared/styled_text.dart';
+import 'package:character_creator/theme.dart';
 import 'package:flutter/material.dart';
 
 class VocationCard extends StatelessWidget {
-  const VocationCard({super.key, required this.vocation});
+  const VocationCard(
+      {super.key,
+      required this.vocation,
+      required this.onTap,
+      required this.selected});
 
   final Vocation vocation;
+  final void Function(Vocation) onTap;
+  final bool selected;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        child: Padding(
-            padding: const EdgeInsets.all(8),
-            child: Row(children: [
-              Image.asset(
-                'assets/img/vocations/${vocation.image}',
-                width: 80,
-                colorBlendMode: BlendMode.color,
-              ),
-              const SizedBox(width: 10),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    StyledHeading(vocation.title),
-                    StyledText(vocation.description)
-                  ],
+    return GestureDetector(
+      onTap: () {
+        onTap(vocation);
+      },
+      child: Card(
+          color: selected ? AppColors.secondaryColor : Colors.transparent,
+          child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(children: [
+                Image.asset(
+                  'assets/img/vocations/${vocation.image}',
+                  width: 80,
+                  colorBlendMode: BlendMode.color,
                 ),
-              )
-            ])));
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      StyledHeading(vocation.title),
+                      StyledText(vocation.description)
+                    ],
+                  ),
+                )
+              ]))),
+    );
   }
 }
