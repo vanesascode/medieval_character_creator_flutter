b0b8162fe95c02fe191550c21d17acfc651abfcf
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-08 20:26:38 +0200

    feat: :sparkles: add the profile screen for each of the characters

---
+++ lib/screens/profile/profile.dart
@@ -0,0 +1,50 @@
+import 'package:character_creator/models/character.dart';
+import 'package:character_creator/shared/styled_text.dart';
+import 'package:character_creator/theme.dart';
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({
+    super.key,
+    required this.character,
+  });
+
+  final Character character;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: StyledTitle(character.name)),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              color: AppColors.secondaryColor.withOpacity(0.3),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/img/vocations/${character.vocation.image}',
+                    width: 140,
+                    height: 140,
+                  ),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        StyledHeading(character.vocation.title),
+                        StyledText(character.vocation.description),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
