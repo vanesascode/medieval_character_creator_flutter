26fd0e8bc9fd269421eec4e2e4802424eb182386
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-18 17:32:04 +0200

    refactor: :recycle: fix double quotes in the StyledText widget

--- lib/shared/styled_text.dart
+++ lib/shared/styled_text.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import "package:flutter/material.dart";
+import "package:google_fonts/google_fonts.dart";
 
 class StyledText extends StatelessWidget {
   const StyledText(this.text, {super.key});
