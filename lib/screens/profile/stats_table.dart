75d6cd31e11e4492c63ac62257d4154613aaf97f
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-18 16:39:50 +0200

    feat: :sparkles: add functionality to the arrows in the stats_table widget

--- lib/screens/profile/stats_table.dart
+++ lib/screens/profile/stats_table.dart
@@ -16,53 +16,74 @@ class _StatsTableState extends State<StatsTable> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(children: [
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
           Container(
-            color: AppColors.secondaryColor,
-            padding: const EdgeInsets.all(8),
-            child: Row(children: [
-              Icon(Icons.star,
-                  color: widget.character.points > 0
-                      ? Colors.yellow
-                      : Colors.grey),
-              const SizedBox(width: 20),
-              const StyledText('Stat points available:'),
-              const Expanded(child: SizedBox(width: 20)),
-              StyledHeading(widget.character.points.toString())
-            ]),
-          ),
+              color: AppColors.secondaryColor,
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: widget.character.points > 0
+                        ? Colors.yellow
+                        : Colors.grey,
+                  ),
+                  const SizedBox(width: 20),
+                  const StyledText('Stat points available:'),
+                  const Expanded(child: SizedBox()),
+                  StyledHeading(widget.character.points.toString())
+                ],
+              )),
           Table(
-            children: widget.character.statsAsFormattedList.map((stat) {
-              return TableRow(children: [
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: StyledHeading(stat['title']!)),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: StyledHeading(stat['value']!)),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: IconButton(
-                      icon:
-                          Icon(Icons.arrow_upward, color: AppColors.textColor),
-                      onPressed: () {}),
-                ),
-                TableCell(
+              children: widget.character.statsAsFormattedList.map((stat) {
+            return TableRow(
+                decoration: BoxDecoration(
+                    color: AppColors.secondaryColor.withOpacity(0.5)),
+                children: [
+                  TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: StyledHeading(stat['title']!),
+                    ),
+                  ),
+                  TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: StyledHeading(stat['value']!),
+                    ),
+                  ),
+                  TableCell(
                     verticalAlignment: TableCellVerticalAlignment.middle,
                     child: IconButton(
-                        icon: Icon(Icons.arrow_downward,
-                            color: AppColors.textColor),
-                        onPressed: () {}))
-              ]);
-            }).toList(),
-          )
-        ]));
+                      icon:
+                          Icon(Icons.arrow_upward, color: AppColors.textColor),
+                      onPressed: () {
+                        setState(() {
+                          widget.character.increaseStat(stat['title']!);
+                        });
+                      },
+                    ),
+                  ),
+                  TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: GestureDetector(
+                      child: Icon(Icons.arrow_downward,
+                          color: AppColors.textColor),
+                      onTap: () {
+                        setState(() {
+                          widget.character.decreaseStat(stat['title']!);
+                        });
+                      },
+                    ),
+                  ),
+                ]);
+          }).toList()),
+        ],
+      ),
+    );
   }
 }
