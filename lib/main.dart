dc070e905cae2c6549beae89b16cae0af2deacca
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-24 20:18:51 +0200

    feat: :sparkles: wrap the home screen with the store provider in the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import "package:character_creator/screens/home/home.dart";
+import "package:character_creator/services/character_store.dart";
 import "package:character_creator/theme.dart";
 import "package:flutter/material.dart";
+import "package:provider/provider.dart";
 
 void main() {
-  runApp(MaterialApp(
-    theme: primaryTheme,
-    home: const Home(),
+  runApp(ChangeNotifierProvider(
+    create: (context) => CharacterStore(),
+    child: MaterialApp(
+      theme: primaryTheme,
+      home: const Home(),
+    ),
   ));
 }
