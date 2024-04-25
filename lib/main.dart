ab877c25204098d2fa182390d3abe564d450cdd3
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-25 19:49:43 +0200

    feat: :sparkles: add Firebase packages and initialization code in the main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import "package:character_creator/services/character_store.dart";
 import "package:character_creator/theme.dart";
 import "package:flutter/material.dart";
 import "package:provider/provider.dart";
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ChangeNotifierProvider(
     create: (context) => CharacterStore(),
     child: MaterialApp(
