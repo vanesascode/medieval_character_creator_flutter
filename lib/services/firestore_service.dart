eec8dcd6b2f0baa4084647b7b446d559f128ba15
vanesa <vanesa.juarez.paris@gmail.com>
2024-05-05 19:17:47 +0200

    feat: :sparkles: add a withConverter method in the firestore_service file

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,5 +1,11 @@
+import 'package:character_creator/models/character.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreService {
-  static final ref = FirebaseFirestore.instance.collection('characters');
+  static final ref = FirebaseFirestore.instance
+      .collection('characters')
+      .withConverter(
+          fromFirestore: Character.fromFirestore,
+          toFirestore: (Character character, options) =>
+              character.toFirestore());
 }
