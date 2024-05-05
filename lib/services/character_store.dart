8a7773cb6b887f89f0a611dd3b88dcf8b3745256
vanesa <vanesa.juarez.paris@gmail.com>
2024-05-05 19:48:29 +0200

    feat: :sparkles: add the fetchCharacters function in the store and delete de dummy data

--- lib/services/character_store.dart
+++ lib/services/character_store.dart
@@ -1,28 +1,9 @@
 import 'package:character_creator/models/character.dart';
-import 'package:character_creator/models/vocation.dart';
 import 'package:character_creator/services/firestore_service.dart';
 import 'package:flutter/material.dart';
 
 class CharacterStore extends ChangeNotifier {
-  final List<Character> _characters = [
-    Character(
-        id: "1", name: "Klara", vocation: Vocation.wizard, slogan: "Kapumf!"),
-    Character(
-        id: "2",
-        name: "Jonny",
-        vocation: Vocation.junkie,
-        slogan: "Light me up..."),
-    Character(
-        id: "3",
-        name: "Crimson",
-        vocation: Vocation.raider,
-        slogan: "Fire in the hole!"),
-    Character(
-        id: "4",
-        name: "Shaun",
-        vocation: Vocation.ninja,
-        slogan: "Alright then gang."),
-  ];
+  final List<Character> _characters = [];
 
   get characters => _characters;
 
@@ -30,5 +11,15 @@ class CharacterStore extends ChangeNotifier {
     FirestoreService.addCharacter(character);
     _characters.add(character);
     notifyListeners();
+  }
+
+  void fetchCharactersOnce() async {
+    if (characters.length == 0) {
+      final snapshot = await FirestoreService.getCharactersOnce();
+      for (var doc in snapshot.docs) {
+        _characters.add(doc.data());
+      }
+      notifyListeners();
+    }
   }
 }
