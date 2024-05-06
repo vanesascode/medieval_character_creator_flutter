19c9babf7fcb902847702fc1fcd2c1a659fce55e
vanesa <vanesa.juarez.paris@gmail.com>
2024-05-06 09:41:34 +0200

    feat: :sparkles: add the Hero widget to animate the image from the profile screen to the character_card screen

--- lib/screens/home/character_card.dart
+++ lib/screens/home/character_card.dart
@@ -16,8 +16,12 @@ class CharacterCard extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
       child: Row(
         children: [
-          Image.asset("assets/img/vocations/${character.vocation.image}",
-              width: 80),
+          Hero(
+            tag: character.id.toString(),
+            child: Image.asset(
+                "assets/img/vocations/${character.vocation.image}",
+                width: 80),
+          ),
           const SizedBox(width: 20),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
